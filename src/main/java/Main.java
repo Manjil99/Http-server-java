f30c36587ddbd2a6152b3656629b55ff11efe2df
Manjil <choudhurymanjil18@gmail.com>
2024-06-18 17:54:45 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,9 +16,11 @@ public class Main {
        serverSocket = new ServerSocket(4221);
        // Since the tester restarts your program quite often, setting SO_REUSEADDR
        // ensures that we don't run into 'Address already in use' errors
+
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
