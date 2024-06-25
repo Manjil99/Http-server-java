35b3c2a794a1d965c734962379055111d7b84926
Manjil <choudhurymanjil18@gmail.com>
2024-06-25 21:17:31 +0530

    pass stage

---
+++ src/main/java/HttpServer.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HttpServer {
+    private final ExecutorService executorService;
+    public HttpServer(final int concurrencyLevel){
+        executorService = Executors.newFixedThreadPool(concurrencyLevel);
+    }
+    public void run() {
+
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(4221);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+
+            serverSocket.setReuseAddress(true);
+            while(true) {
+                Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+                this.executorService.submit(() -> {
+                    try {
+                        handleRequest(clientSocket);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    private void handleRequest(Socket clientSocket) throws IOException {
+        System.out.println("accepted new connection");
+        InputStream input = clientSocket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String line = reader.readLine();
+        String[] HttpRequest = line.split(" ");
+        OutputStream output = clientSocket.getOutputStream();
+        if (HttpRequest[1].matches("/echo/(.*)")) {
+            String responseString = HttpRequest[1].substring(6);
+            output.write(("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + responseString.length()
+                    + "\r\n\r\n" + responseString).getBytes());
+        } else if (HttpRequest[1].equals("/user-agent")) {
+            reader.readLine();
+            String userAgent = reader.readLine().split(" ")[1];
+            output.write(("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.length()
+                    + "\r\n\r\n" + userAgent).getBytes());
+
+        } else if (HttpRequest[1].equals("/")) {
+            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        } else {
+            output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+        }
+        clientSocket.close();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,11 @@
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
-
-     try {
-       serverSocket = new ServerSocket(4221);
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-       InputStream input = clientSocket.getInputStream();
-       BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-       String line = reader.readLine();
-       String[] HttpRequest = line.split(" ");
-       OutputStream  output = clientSocket.getOutputStream();
-       if(HttpRequest[1].matches("/echo/(.*)")){
-           String responceString = HttpRequest[1].substring(6);
-           output.write(("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + responceString.length()
-                   + "\r\n\r\n" + responceString).getBytes());
-       } else if(HttpRequest[1].equals("/user-agent")){
-           reader.readLine();
-           String userAgent = reader.readLine().split(" ")[1];
-           output.write(("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.length()
-                   + "\r\n\r\n" + userAgent).getBytes());
-
-       }else if(HttpRequest[1].equals("/")) {
-           output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-       } else {
-           output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-       }
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
+      // You can use print statements as follows for debugging, they'll be visible when running tests.
+      System.out.println("Logs from your program will appear here!");
+      final HttpServer httpServer = new HttpServer(5);
+      httpServer.run();
   }
 }
